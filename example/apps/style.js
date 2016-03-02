2f384b47ce841835b6c05355ffdc3ee1a2f6d5e1
landlessness <brian@landlessness.net>
2016-03-01 22:32:28 -0500

    putting state image under style

--- example/apps/style.js
+++ example/apps/style.js
@@ -6,17 +6,19 @@ var DOOR_OPENED_IMAGE_URL = 'https://cdn0.iconfinder.com/data/icons/mobile-devel
 module.exports = function(server) {
   var doorSensorQuery = server.where({ type: 'door' });
   server.observe([doorSensorQuery], function(doorSensor){
-    // add property to track state image
-    doorSensor.stateImage = DOOR_CLOSED_IMAGE_URL;
+    doorSensor.style = {};
     // wrap it in a WebSockets monitor
-    doorSensor._initMonitor('stateImage');
-    doorSensor._monitors.push('stateImage');
+    doorSensor._initMonitor('style');
+    doorSensor._monitors.push('style');
+    
+    // add property to track state image
+    doorSensor.style.stateImage = DOOR_CLOSED_IMAGE_URL;
     // change the state image when door changes state
     doorSensor.on('force-mock-close', function(s) {
-      doorSensor.stateImage = DOOR_CLOSED_IMAGE_URL;
+      doorSensor.style = {stateImage: DOOR_CLOSED_IMAGE_URL};
     });
     doorSensor.on('force-mock-open', function(s) {
-      doorSensor.stateImage = DOOR_OPENED_IMAGE_URL;
+      doorSensor.style = {stateImage: DOOR_OPENED_IMAGE_URL};
     });
    }
   );
